05043646993a1098f5dce112fe6042ca0e5edec6
adrnagstn <79967454+adrnagstn@users.noreply.github.com>
2021-03-29 22:14:51 +0800

    Add files via upload

---
+++ market.php
@@ -0,0 +1,10 @@
+<?php
+$pageTitle = "Market";
+require('nav.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+MARKET
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="includes/css/mainlib.css">
+    <title><?=$pageTitle?></title>
+</head>
+<body>
+<header>
+    <!-- <img class="logo" src="includes/images/TraderShedLogoNavBar.svg"> -->
+    <nav>
+           <ul class="nav__links">
+                <li><a href ="#">Home</li>
+                <li><a href ="#">Services</li>
+                <li><a href ="#">Market</li>
+                <li><a href ="#">Search</li>
+                <li><a href ="#">Login</li>
+            </ul>
+    </nav>
+    <a class="cta" href="#"><button>Contact</button></a>
+    <header>
+</body>
+</html>
